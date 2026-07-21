166b5d1fefcf7ca17cb00df13b2b35205d2ed6f2
armouredmaoG <rajashri674@gmail.com>
2026-07-21 15:53:48 +0530

    test animation check home v4

--- codes/common/text-anim.js
+++ codes/common/text-anim.js
@@ -72,7 +72,7 @@ function homeEntranceAnimation(headingWrap, secondaryWrap = null, endColor = "#f
                         duration: 0.5,
                         ease: "power1.out",
                     },
-                    duration * 0.2
+                    duration * 0.4
                 );
             }
         };
